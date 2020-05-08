b69661c65ab4f760bf4f3847553dfe70645e4bae
Qazi Amaan <qazi9amaan@gmail.com>
2020-05-08 18:13:12 +0530

    AddTransaction

--- src/components/TransactionItem.js
+++ src/components/TransactionItem.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionItem = ({transaction}) => {
+    const {deleteTransaction}  = React.useContext(GlobalContext)
     const sign = transaction.amount < 0 ? '-' : '+';
     const color = transaction.amount < 0 ? 'minus' : 'plus';
     return (
@@ -8,7 +10,7 @@ export const TransactionItem = ({transaction}) => {
              <li className={color}>
                     {transaction.text} 
                     <span>{sign}${Math.abs(transaction.amount)} </span>
-                    <button class="delete-btn">x</button>
+                    <button onClick={()=> deleteTransaction(transaction.id)} class="delete-btn">x</button>
                     </li> 
         </div>
     )
